01cf4e65063401b6e286b9104373dec147466ef4
jramosl <jesuar12@gmail.com>
2017-04-01 11:10:02 -0500

    Update validar.php

--- validar.php
+++ validar.php
@@ -7,11 +7,12 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="/stls.css">
+        <link rel="stylesheet" href="stls.css">
     </head>
     <body>
         <?php
-        require_once 'conexionBD.php';
+        echo "PHP está funcionado!!!";
+        require_once './conexionBD.php';
         $nombre = "";
         $obj = new conexionBD();
         $enlace = $obj->crearConexion();
