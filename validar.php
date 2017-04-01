d79347686527028d1c3d02a229e6e9a7f73f9d01
jramosl <jesuar12@gmail.com>
2017-04-01 11:31:31 -0500

    Update validar.php

--- validar.php
+++ validar.php
@@ -11,15 +11,21 @@
     </head>
     <body>
         <?php
-        echo "PHP está funcionado!!!";
+        echo "PHP está funcionado1!!!";
         require_once 'conexionBD.php';
+        echo "PHP está funcionado2!!!";
         $nombre = "";
         $obj = new conexionBD();
+        echo "PHP está funcionado3!!!";
         $enlace = $obj->crearConexion();
+        echo "PHP está funcionado4!!!";
         $user = $_POST['usuario'];
         $pass = $_POST['pass'];
+        echo "PHP está funcionado5!!!";
         $result = mysqli_query($enlace, 'SELECT username,pass FROM usuario where username=' . '"' . $user . '" && pass=' . '"' . $pass . '"');
+        echo "PHP está funcionado6!!!";
         $rows = mysqli_num_rows($result);
+        echo "PHP está funcionado7!!!";
         if ($rows == 1) {
             while ($res = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
                 $nombre = $res['username'];
