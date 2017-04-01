7ed8a179f5cd8ce13c96aa20c95413fbdaefcfce
jramosl <jesuar12@gmail.com>
2017-04-01 13:01:31 -0500

    Update validar.php

--- validar.php
+++ validar.php
@@ -13,6 +13,8 @@
         <?php
         echo 'PHP está funcionado1!!!';
         phpinfo();
+        $msq=new mysqli("us-cdbr-iron-east-03.cleardb.net","bb87881e7a2166","dc7e8234","ad_643556e1444d091");
+        echo 'PHP está funcionado10!!!';
         $link = mysqli_connect("us-cdbr-iron-east-03.cleardb.net", "bb87881e7a2166", "dc7e8234") or die("No se pudo conectar: " . mysqli_error());
         echo 'PHP está funcionado2!!!';
         $conexion = mysqli_select_db($link, 'ad_643556e1444d091') or die('No se pudo seleccionar la base de datos');
