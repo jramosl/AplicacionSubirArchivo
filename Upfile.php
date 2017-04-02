3c9e0ca070777bd23ab692b08f3e53692b09699f
jramosl <jesuar12@gmail.com>
2017-04-01 20:05:40 -0500

    Update Upfile.php

--- Upfile.php
+++ Upfile.php
@@ -5,10 +5,9 @@ $name_file = basename($_FILES['archivo']['name']);
 $usr = $_SESSION['usuario'];
 $target_path = $target_path . $name_file;
 if (move_uploaded_file($_FILES['archivo']['tmp_name'], $target_path)) {
-    require_once './conexionBD.php';
-    $obj = new conexionBD();
-    $enlace = $obj->crearConexion();
-    mysqli_query($enlace, "INSERT INTO archivo(nombre,userid) VALUES('$name_file','$usr')");
+    $link = mysqli_connect('us-cdbr-iron-east-03.cleardb.net', 'bb87881e7a2166', 'dc7e8234') or die('No se pudo conectar: ' . mysqli_error());
+    $conexion = mysqli_select_db($link, "ad_643556e1444d091") or die('No se pudo seleccionar la base de datos');
+    mysqli_query($link, "INSERT INTO archivo(nombre,userid) VALUES('$name_file','$usr')");
     header("Location: IndexUser.php?op=ok");
 } else {
     header("Location: IndexUser.php?op=er");
